e045a44359a833e74944889f649fda1a26ba725a
cgollub <gollubchristina@gmail.com>
2024-12-06 14:35:04 +0100

    woosh library with stemming

--- week2/wooshdemo.py
+++ week2/wooshdemo.py
@@ -1,34 +1,37 @@
 from whoosh.index import create_in
-from whoosh.fields import *
+from whoosh.fields import Schema, TEXT
 
-# Here, the structure of index entires is defined. You can add more fields with metadata, computed values etc.,
-# and use them for searching and ranking. 
-# We only use a title and a text.
-#
-# The "stored" attribute is used for all parts that we want to be able to fully retrieve from the index
-#
-# schema = Schema(title=TEXT(stored=True), content=TEXT)
+#added following: 
+# Define the schema for the index
+# The schema specifies the fields you will store and search 
+schema = Schema(title=TEXT(stored=True), content=TEXT)
 
-# Create an index in the directory indexdr (the directory must already exist!)
+# Create an index in the "indexdir" directory (the directory must already exist!)
+import os
+if not os.path.exists("indexdir"):
+    os.mkdir("indexdir")
+
+# Create the index
 ix = create_in("indexdir", schema)
 writer = ix.writer()
 
-# now let's add some texts (=documents)
+# Add some documents to the index
 writer.add_document(title=u"First document", content=u"This is the first document we've added!")
 writer.add_document(title=u"Second document", content=u"The second one is even more interesting!")
 writer.add_document(title=u"Songtext", content=u"Music was my first love and it will be the last")
 
-# write the index to the disk
+# Write the index to the disk
 writer.commit()
 
 # Retrieving data
 from whoosh.qparser import QueryParser
+
+# Perform a search
 with ix.searcher() as searcher:
-    # find entries with the words 'first' AND 'last'
+    # Search for entries containing the words 'first' AND 'last'
     query = QueryParser("content", ix.schema).parse("first last")
     results = searcher.search(query)
 
-    # print all results
+    # Print all results
     for r in results:
         print(r)
-        
