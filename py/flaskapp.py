da759c87e5ad1220b31544fc0f177114e147f96d
iritter <isabel.ritter@dfki.de>
2024-12-09 14:24:46 +0100

    connect flaskapp and index.dir

--- py/flaskapp.py
+++ py/flaskapp.py
@@ -1,13 +1,24 @@
 from flask import Flask, request, render_template
+from whoosh.index import open_dir
+from whoosh.qparser import QueryParser
+import os
 
-app = Flask(__name__)
+# dynamically determine the path to the templates directory
+current_dir = os.path.dirname(os.path.abspath(__file__))  # directory of flaspapp.py
+template_dir = os.path.join(current_dir, "../templates")  #navigate to templates folder
 
+app = Flask(__name__, template_folder=template_dir)
+
+"""
 # exemplatory search index
 search_index = {
     "python": ["https://www.python.org/", "https://docs.python.org/3/"],
     "flask": ["https://flask.palletsprojects.com/", "https://realpython.com/tutorials/flask/"],
     "web development": ["https://developer.mozilla.org/en-US/docs/Learn", "https://www.w3schools.com/"]
-}
+}"""
+
+# Open the Whoosh index
+search_index = open_dir("indexdir")
 
 # start page
 @app.route("/")
@@ -20,18 +31,44 @@ def reversed():
     rev = request.args['rev'][::-1]
     return render_template('reversed.html', rev=rev)
 
+# home page
 @app.route("/home", methods=['GET'])
 def home():
     # render a search fomr
     return render_template('home.html')
 
+
+# search results
 @app.route("/search", methods=['GET'])
 def search():
+    """
+    Perform a search query using Whoosh index.
+    """
     # retrieve the search query
     query = request.args.get('q', '').lower()
+    results = []
+
     # perform the search
-    results = search_index.get(query, [])
+    if query:
+        with search_index.searcher() as searcher:
+            qp = QueryParser("content", search_index.schema)  # Assuming "content" is the field you want to search
+            parsed_query = qp.parse(query)
+            whoosh_results = searcher.search(parsed_query)
+
+            # Collect results
+            for r in whoosh_results:
+                results.append({"url": r["url"], "title": r["title"]})
+
     return render_template('search.html', query=query, results=results)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5500)  # port5500
+
+
+
+
+
+
+
+
