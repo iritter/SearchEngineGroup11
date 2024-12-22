903006b3f8d8daf3ce08c4765756eced36ab08e4
jt0051 <johanna.trowitz@outlook.com>
2024-12-22 22:35:42 +0100

    highlights stemming field_boost

--- py/crawlmitindex.py
+++ py/crawlmitindex.py
@@ -1,23 +1,26 @@
 from whoosh.index import create_in
-from whoosh.fields import Schema, TEXT, ID
-from whoosh.qparser import QueryParser
+from whoosh.fields import Schema, TEXT, ID, KEYWORD
+from whoosh.analysis import StemmingAnalyzer
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 import logging
-import re
-from collections import defaultdict
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
 import os
 
-# Define the schema for the index
-# The schema specifies the fields you will store and search 
-schema = Schema(url=ID(stored=True), title=TEXT(stored=True), content=TEXT(stored=True), keywords=TEXT(stored=True), description=TEXT(stored=True))
+# Whoosh StemmingAnalyzer 
+stem_ana = StemmingAnalyzer()
 
+# Schema for the index, specifies the fields, the analyzer and field_boost (weight)
+schema = Schema(
+    url=ID(stored=True), 
+    title=TEXT(analyzer=stem_ana, stored=True, field_boost=2.5),
+    h1_text=TEXT(analyzer=stem_ana, stored=True, field_boost=3.0),
+    content=TEXT(analyzer=stem_ana, stored=True, field_boost=2.0), 
+    keywords=KEYWORD(analyzer=stem_ana, stored=True, field_boost=1.0), 
+    description=TEXT(analyzer=stem_ana, stored=True, field_boost=1.0)
+)
 
 # Create an index in the "indexdir" directory (the directory must already exist!)
-
 if not os.path.exists("indexdir"):
     os.mkdir("indexdir")
 
@@ -28,104 +31,39 @@ writer = ix.writer()
 # Write the index to the disk
 writer.commit()
 
-def add_to_index(writer, url, title, content, keywords, description):
+def add_to_index(writer, url, title, content, keywords, description, h1_text):
     """
-    Add a document to the Whoosh index.
+    Adds a document to the Whoosh index
+    Maps field names in the schema to the provided arguments
+    Returns:
+        None
     """
-    writer.add_document(url=url, title=title, content=content, keywords=keywords, description=description)
+    writer.add_document(
+        url=url, 
+        title=title, 
+        content=content, 
+        keywords=keywords, 
+        description=description, 
+        h1_text=h1_text
+    )
 
-
-# perform a search
-def perform_search(q_words):
-    """
-    Perform a search for the given query words.
-    """
-    with ix.searcher() as searcher:
-        query = QueryParser("content", ix.schema).parse(q_words)
-        results = searcher.search(query)
-
-        # Collect results within the context manager
-        output = []
-        for r in results:
-            output.append((r['url'], r['title']))  # Retrieve URL and title from stored fields
-
-        return output
-
-
-#logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
-def cook(soup_text):
-    """tokenize the page's content using BeautifulSoup tool"""
-
-    
-    soup_text = soup_text.get_text(separator=' ')
-
-    # FRAGE: 
-    # Falls wir keine Zahlen dabei haben wollen?!
-    #soup_is_ready = [word for word in re.findall(r'\b\w+\b', soup_text.lower()) if not word.isdigit()]
-    soup_is_ready = re.findall(r'\b\w+\b', soup_text.lower())
-    return soup_is_ready
-
-# Initialize stemmer and lemmatizer
-stemmer = PorterStemmer()
-lemmatizer = WordNetLemmatizer()
-
-
-def process_query(query, use_stemming=False, use_lemmatization=False):
-    """ 
-    Für die Query word können wir auch Tokenization, lemmization und so einfügen, 
-    damit die suche besser wird, würde ich aber erst ganz am Ende machen, also in 2 wochen - je nachdem was
-    wir noch so machen... und dann hier in die function!!!
-    Applying stemming and/or lemmatization.
-    """
-    processed_words = []
-    for word in query:
-        if use_stemming:
-            processed_words.append(stemmer.stem(word))
-        elif use_lemmatization:
-            processed_words.append(lemmatizer.lemmatize(word))
-        else:
-            processed_words.append(word)
-    return processed_words
-
-
-def index(url, word_to_urls, url_to_words, all_words):
-
-    """
-    Build an in-memory index of words to URLs, and extract the words for a URL.
-    :param url: URL for which index shall be generated
-    :word_to urls: list of URLS
-    :url_to_words mapping: list of words on a page
-    :all_words: total set of unique words across all pages
-    """
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    words = cook(soup)
-    url_to_words[url] = words
-    all_words.update(words)
-    for word in set(words):
-        word_to_urls[word].add(url)
-    
-    return soup
 
 def crawl(initial_url):
 
     """
-    Crawl a website and build index function, and store the words for each individual URL.
-    
-    :param initial_url: URL to start crawling from.
-    :return: Tuple of two dictionaries
-             - word-to-URLs index (word -> list of URLs)
-             - URL-to-words mapping (URL -> list of words)
-             - total set of unique words across all pages
+    Crawl a website and build index
+    - starts with initial_url, stays in base domain
+    - processes stored metadata of pages
+
+    Param:
+        initial_url (str): The starting URL for the crawl
+    Returns:
+        None
     """
 
     visited = set()
     agenda = [initial_url]
     base_url = "{uri.scheme}://{uri.netloc}".format(uri=urlparse(initial_url))
-    word_to_urls = defaultdict(set)
-    url_to_words = {}
-    all_words = set()
 
     # Open the writer for the duration of the crawl
     with ix.writer() as writer:
@@ -139,28 +77,12 @@ def crawl(initial_url):
                 response = requests.get(url)
                 # Ensure the response is valid and HTML content
                 if response.status_code == 200 and 'text/html' in response.headers.get('Content-Type', ''):
+                    
                     # Ensure the correct encoding
                     response.encoding = response.apparent_encoding
 
                     # Create the BeautifulSoup object
                     soup = BeautifulSoup(response.text, 'html.parser')
-
-                    # Extract the title
-                    title = soup.title.string.strip() if soup.title and soup.title.string else "No title"
-                    print(f"Extracted Title: {title}")
-
-                    # Extract the page content
-                    content = soup.get_text(separator=' ', strip=True)
-                    
-                    # earch-specific metadata explicitly defined by the website's creator
-                    keywords_meta = soup.find('meta', {'name': 'keywords'})
-                    keywords = keywords_meta.get('content', "") if keywords_meta else ""
-
-                    description_meta = soup.find('meta', {'name': 'description'})
-                    description = description_meta.get('content', "") if description_meta else ""
-
-                    # Add the page to the index
-                    add_to_index(writer, url, title, content, keywords, description)
 
                     for link in soup.find_all('a', href=True): # get all links on that page
                         absolute_url = urljoin(url, link['href'])
@@ -168,48 +90,52 @@ def crawl(initial_url):
                         if absolute_url.startswith(base_url) and absolute_url not in visited:
                             agenda.append(absolute_url) # append it to the agenda
 
+                    # Extract the title
+                    title = soup.title.string.strip() if soup.title and soup.title.string else "No title"
+                    print(f"Extracted Title: {title}")
+
+                    # Extract h1
+                    try:
+                        body = soup.find('body')
+                        h1 = body.find('h1') if body else None
+                        h1_text = h1.get_text() if h1 else None
+
+                    except Exception as e:
+                        print(f"Error fetching {url}: {e}")
+                        h1_text = None
+                    
+                    # Extract metadata keywords and description (if created)
+                    keywords_meta = soup.find('meta', {'name': 'keywords'})
+                    keywords = keywords_meta.get('content', "") if keywords_meta else "No keywords found"
+                    print(f"Extracted Keywords: {keywords}")
+
+                    description_meta = soup.find('meta', {'name': 'description'})
+                    description = description_meta.get('content', "") if description_meta else "No description found"
+                    print(f"Extracted Description: {description}")
+
+                    # Remove a-tags
+                    for a_tag in soup.find_all('a'):
+                        a_tag.decompose()
+
+                    # Extract the page content with BeautifulSoup
+                    content = soup.get_text(separator=' ', strip=True)
+                    
+                    # Add the page to the index
+                    add_to_index(writer, url, title, content, keywords, description, h1_text)
+
             except requests.RequestException as e:
                 logging.error(f"Failed to fetch: {url} Error: {e}")
+    return
 
-    # return the words ad lists and number of uniqe words
-    word_to_urls = {word: list(urls) for word, urls in word_to_urls.items()}
-    return word_to_urls, url_to_words, all_words
-
-
-def search(index, words):
-    """
-    Search for URLs containing all of the given words.
-
-    :param index: The in-memory index (word -> list of URLs).
-    :param words: List of words to search for.
-    :return: List of URLs that contain all the words.
-    """
-    if not words:
-        return []
-
-    # Process the query word
-    word = process_query(words[0], use_stemming=True, use_lemmatization=True)
-    result_urls = set(index.get(words[0], []))  # get urls for the first word
-
-    # intersect with urlss for each subsequent word
-    for word in words[1:]:
-        word = process_query(word, use_stemming=True, use_lemmatization=True)
-        result_urls &= set(index.get(word, []))  # set intersection
-
-    return list(result_urls)
 
 if __name__ == "__main__":
-    # initial url, url to be crawled
+    # url to be crawled
     initial_url = 'https://vm009.rz.uos.de/crawl/index.html'
+    #initial_url = 'https://www.ikw.uni-osnabrueck.de/en/home.html'
 
-    word_to_urls, url_to_words, all_words = crawl(initial_url)
-    
-    # Perform a search
-    result = perform_search("symbol virgin")
-    
-    # Display the results
-    print("Search Results:")
-    for url, title in result:
-        print(f"URL: {url}, Title: {title}")
+    # start crawling
+    crawl(initial_url)
+
+
 
 
--- py/flaskapp.py
+++ py/flaskapp.py
@@ -1,79 +1,80 @@
 from flask import Flask, request, render_template
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
+from whoosh.analysis import StemmingAnalyzer
 import os
-from bs4 import BeautifulSoup
-import requests
+import re
 
 # dynamically determine the path to the templates directory
-current_dir = os.path.dirname(os.path.abspath(__file__))  # directory of flaspapp.py
-template_dir = os.path.join(current_dir, "../templates")  #navigate to templates folder
-static_dir = os.path.join(current_dir, "../static")
+current_dir = os.path.dirname(os.path.abspath(__file__))  # directory of flaskapp.py
+template_dir = os.path.join(current_dir, "../templates")  # navigate to templates folder
+static_dir = os.path.join(current_dir, "../static") # navigate to static folder
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 # Open the Whoosh index
 search_index = open_dir("indexdir")
 
+# Whoosh StemmingAnalyzer 
+stem_ana = StemmingAnalyzer()
+
 # home page
 @app.route("/home/", methods=['GET'])
-def home2():
+def home():
     # render a search fomr
     return render_template('home.html')
-import re
 
+# fallback to home page
+@app.route("/", methods=['GET'])
+def home2():
+    return render_template('home.html')
+
+# search page to show results
 @app.route("/search/", methods=['GET'])
 def search():
     """
-    Perform a search query using Whoosh index.
+    Handling of search requests.
+
+    - Retrieves search query from the request arguments
+    - Uses Whoosh to process and execute search query
+    - Highlights query words in the title, h1, and teaser
+
+    Returns:
+        - original query
+        - search results to be rendered in the 'search.html' template
+            consisting of url, title, keywords, description and teaser
+
     """
     # retrieve the search query
-    query = request.args.get('q', '').lower()
+    query = request.args.get('q', '')
     results = []
 
     # perform the search
     if query:
         with search_index.searcher() as searcher:
-            qp = QueryParser("content", search_index.schema)
-            parsed_query = qp.parse(query)
+
+            stemmed_query, parsed_query = process_query(query)
             whoosh_results = searcher.search(parsed_query)
 
             # Process each result only once
             for r in whoosh_results:
-                url = r["url"]
-                title = r["title"]
-                keywords = r.get("keywords", "No keywords")
-                description = r.get("description", "No description available")
-                content = r["content"]
+                url = r.get("url", "")
+                title = r.get("title", "")
+                h1_text = r.get("h1_text", "")
+                keywords = r.get("keywords", "")
+                description = r.get("description", "")
+                content = r.get("content", "")
 
-                # Try to fetch the page and extract <h1> from the body
-                try:
-                    response = requests.get(url)
-                    soup = BeautifulSoup(response.content, 'html.parser')
+                # Extract the teaser
+                teaser = extract_teaser(content, stemmed_query, h1_text, title)
 
-                    # Find the <body> tag
-                    body = soup.find('body')
-                    h1 = body.find('h1') if body else None
-                    h1_text = h1.get_text() if h1 else None
-
-                except Exception as e:
-                    print(f"Error fetching {url}: {e}")
-                    h1_text = None
-
-                # Extract the teaser and highlight query words
-                teaser = extract_teaser(content, query)
-                teaser = highlight_query_in_teaser(teaser, query)
-
-                # Exclude title or h1 from teaser if present
-                if h1_text:
-                    teaser = teaser.replace(h1_text, "").replace(title, "").strip()
-                else:
-                    teaser = teaser.replace(title, "").strip()
+                # highlight query in teaser and title (or h1 if existing)
+                title, teaser = highlight_query_words(h1_text or title, teaser, query, stemmed_query)
 
                 # Append result
                 results.append({
                     "url": url,
-                    "title": h1_text or title,
+                    "title": title,
                     "keywords": keywords,
                     "description": description,
                     "teaser": teaser,
@@ -82,31 +83,111 @@ def search():
     return render_template('search.html', query=query, results=results)
 
 
-def extract_teaser(content, query):
+def process_query(query):
     """
-    Extract a sentence that contains the query word(s) to form a teaser.
+    Processes the search query by stemming and parsing it for Whoosh
+    Param:
+        query (str): The original search query entered by the user
+    Returns:
+        - stemmed_query (list): A list of stemmed words from the query
+        - parsed_query (Query): A Whoosh query object ready for searching
     """
-    # Split the content into sentences (you can use regex or any other sentence split method)
+
+    print("original query: ", query)
+    # Whoosh QueryParser
+    qp = QueryParser("content", search_index.schema)
+
+    # stemming with Whooosh StemmingAnalyzer
+    stemmed_query = [word.text for word in stem_ana(query)]
+    print("stemmed_query: ", stemmed_query)
+
+    # parsing with QueryParser
+    parsed_query = qp.parse(" ".join(stemmed_query))
+    print("parsed query: ", parsed_query)
+
+    return stemmed_query, parsed_query
+
+
+def extract_teaser(content, stemmed_query, h1_text, title):
+    """
+    Extracts a teaser from the content based on the query words
+    Param:
+        content (str): The text content to search for teaser sentences
+        stemmed_query (list): A list of stemmed query words
+        h1_text (str): The H1 text of the document
+        title (str): The title of the document
+    Returns:
+        str: A teaser string containing sentences with the query words or a fallback teaser
+
+    """
+    search_words = stemmed_query
+    remaining_words = set(search_words)
+
+    # Split the content into sentences with regex
     sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', content)
+    teaser = []
 
-    # Find the sentence that contains the query word(s)
-    for sentence in sentences:
-        if query in sentence.lower():  # Case-insensitive match for query
-            # Clean up extra spaces and return the sentence with the query
-            return sentence.strip()
+    # make sure that every query word appears in teaser
+    while remaining_words:
+        for word in list(remaining_words):
+            # Find the sentence that contains the query word
+            for sentence in sentences:
+                if word in sentence.lower():  # Case-insensitive match for query
+                    teaser.append(sentence.strip())
+                    # remove words that are covered in teaser so far
+                    remaining_words -= set(w for w in search_words if w in sentence.lower())
+                    break
+            break
 
-    # If no matching sentence is found, return a default teaser or empty string
-    return content[:200].strip()  # Return the first 200 characters of the content as fallback teaser
+        # if all query words are covered
+        if not remaining_words:
+            break
 
-def highlight_query_in_teaser(teaser, query):
+    if teaser:
+        # Join collected sentences with " ... "
+        teaser_complete = " ... ".join(teaser).strip()
+    else:
+        teaser_complete = content[:200].strip()  # Return the first 200 characters of the content as fallback teaser
+    # cut title from teaser
+    if h1_text:
+        teaser_complete = teaser_complete.replace(h1_text, "").replace(title, "").strip()
+    else:
+        teaser_complete = teaser_complete.replace(title, "").strip()
+    return teaser_complete + " ... "
+
+
+def highlight_query_words(title, teaser, original_query, stemmed_query):
     """
-    Highlight the query word(s) in the teaser by wrapping them in a span tag with the class 'highlight'.
+    Highlight the query word(s) in the title or h1 and teaser by wrapping them in a span tag 
+    with the class 'highlight'. Original query words are prioritized for exact matches.
+    Stemmed query highlights handle partial matches when the exact word is not found.
+    Regular expressions are used for precise word boundary detection and flexibility.
+
+    Param:
+        title (str): The title or H1 text to highlight
+        teaser (str): The teaser text to highlight
+        original_query (str): The original search query entered by the user
+        stemmed_query (list): A list of stemmed query words
+    
+    Returns:
+        tuple: containing the updated title and teaser with highlights
+
     """
-    query_words = query.split()  # Split the query into individual words
-    for word in query_words:
-        # Use a case-insensitive replace to highlight all occurrences of the query words
-        teaser = re.sub(r'(' + re.escape(word) + r')', r'<span class="highlight">\1</span>', teaser, flags=re.IGNORECASE)
-    return teaser
+    original_query = original_query.split()
+
+    for original_word, stemmed_word in zip(original_query, stemmed_query):
+
+        # Teaser: Match the original word as a full word
+        teaser = re.sub(rf"(\b{re.escape(original_word)}\b)", r'<span class="highlight">\1</span>', teaser, flags=re.IGNORECASE)
+        # Teaser: Match any word containing the stemmed word
+        teaser = re.sub(rf"(\b\w*{re.escape(stemmed_word)}\w*\b)", r'<span class="highlight">\1</span>', teaser, flags=re.IGNORECASE)
+
+        # Title: Match the original word as a full word
+        title = re.sub(rf"(\b{re.escape(original_word)}\b)", r'<span class="highlight">\1</span>', title, flags=re.IGNORECASE)
+        # Title: Match any word containing the stemmed word
+        title = re.sub(rf"(\b\w*{re.escape(stemmed_word)}\w*\b)", r'<span class="highlight">\1</span>', title, flags=re.IGNORECASE)
+
+    return title, teaser
 
 
 @app.errorhandler(404)
@@ -114,4 +195,4 @@ def page_not_found(error):
     return render_template('not-found.html'), 404
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5500)  # port5500
+    app.run(debug=True, host="0.0.0.0", port=5500)  # port5500 as default
